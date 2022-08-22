b0629a504d60b39e6d0783ea7bc449abe00efe62
MARCELO GOMES CASTANHO <71048436+marcelo-prog@users.noreply.github.com>
2022-08-22 20:27:02 -0300

    ssss

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=., initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Site teste</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Meu site</h1>
+            <img src="documentos/105564_1" alt="">
+
+        </header>
+
+        <nav>
+            <?php
+               
+                $no = "Maria";
+                $n = 5 ;
+                echo  $no. " tem ". $n. " de idade <br> " ;
+                echo " $no tem  $n de idade <br><br>" ;
+
+                $m1 = 5 ;
+
+                $m2 = 8 ;
+
+                $m3 = $m1 + $m2 ;
+
+                echo "O resultado da soma é $m3";
+
+
+
+                
+
+
+
+            ?>
+
+
+        </nav>
+
+        <section>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem laboriosam dolore nobis quia totam dolores aliquid blanditiis ea praesentium, consectetur sapiente! Alias in asperiores impedit cupiditate, illum voluptatibus! Aut, tenetur?</p>
+
+        </section>
+
+        <footer>
+
+        </footer>
+        <script>
+            alert ("Hello World") ;
+        </script>
+        
+    </body>
+
+</html>
+    
